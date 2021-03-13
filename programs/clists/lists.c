60e6ebe8c0fcda04e5ce58ecfdda72ecd3416d21
George Sakketos <sdi2000177@di.uoa.gr>
2021-03-14 01:12:23 +0200

    Update Time

--- programs/clists/lists.c
+++ programs/clists/lists.c
@@ -7,11 +7,28 @@
 void list_stats(List list){
 
     printf("\nSize of list %s : %d\n", getName(list), list_size(list));
+
+    ListNode anode = list_first(list);
+
+    for (int i = 0; i < list_size(list); i++){
+        int* lsnode = list_node_value(list, anode);
+        printf("Node %d: %d\n", i+1, *lsnode);
+        anode = list_next(list, anode);
+    }
+
 }
 
 int main(void){
-    List alist = list_create(free);;
+    List alist = list_create(free);
 
+    int* value = malloc(sizeof(int));
+    *value = 10;
+
+    int* value2 = malloc(sizeof(int));
+    *value2 = 12;
+
+    list_insert_next(alist, LIST_BOF, value);
+    list_insert_next(alist, LIST_BOF, value2);
     list_stats(alist);
 
     list_destroy(alist);
